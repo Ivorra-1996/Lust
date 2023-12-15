0ec2a32cd6946d9bc5bbdf95b50a168857b791a3
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-12-15 15:33:40 -0300

    Creacion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,15 @@
 import { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import VerificacionDeEdad from './components/VerificacionDeEdad';
 
-
-// Esto es un componente con nombre camelname 
-function App() {
+// Esto es un componente con nombre camelname que se puede usar hubs
+const App = () => {
   // Estado...
   // Ciclo de vida -> como interactua con el tiempo, es decir, actualiza el estado tantos segundo...
   // JSX es como el HTML de mi aplicacion, no es HTML es JSX..
-  const [pagina,setPagina] = useState();
-  
+
   return (
-    <div className='d-flex align-items-center justify-content-center vh-100'>
-      <div
-        className="modal show"
-        style={{ display: 'block', position: 'initial',  }}
-      >
-        <Modal.Dialog>
-          <Modal.Body>
-            <Modal.Title>Verificación de edad 🧐</Modal.Title>
-          </Modal.Body>
-
-          <Modal.Body>
-            <p>Este sitio web contiene material restringido a menores de edad, que incluye desnudez y representaciones explícitas de 
-              actividad sexual. Al entrar, afirma que tiene por lo menos 18 años de edad o mayor en la 
-              jurisdicción desde que está accediendo el sitio web para adultos y que da consentimiento en ver contenido sexualmente 
-              explícito.</p>
-          </Modal.Body>
-
-          <Modal.Footer className="justify-content-center" >
-            <Button class="btn btn-light" size="lg">Adulto</Button>
-            <Button variant="dark" size="lg">Menor</Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </div>
-    </div>
+    <VerificacionDeEdad/>
   );
 }
 
